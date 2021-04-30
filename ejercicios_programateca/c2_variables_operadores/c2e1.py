dd9edf0e61b466b4b82aef1969bb30253363c230
Alex Mena <alejandromena@correo.unimet.edu.ve>
2021-04-30 15:28:16 -0400

    Modificar los ejercicios a objetos

--- ejercicios_programateca/c2_variables_operadores/c2e1.py
+++ ejercicios_programateca/c2_variables_operadores/c2e1.py
@@ -1,10 +1,16 @@
 #Ejercicio 1 de Variables, Operadores y Strings
-def new_volume(reservoir, rain):
-    rain -= rain*0.1
-    reservoir += rain
-    reservoir += reservoir*0.05
-    reservoir -= reservoir*0.02
-    reservoir -= 2.5e5
-    print(f"El volumen del reservoirorio es {reservoir}m3")
+class WaterReservoir:
+    def __init__(self, reservoir, rain):
+        self.reservoir = reservoir
+        self.rain = rain
+    def new_volume(self):
+        rain_changed = self.rain
+        reservoir_changed = self.reservoir
+        rain_changed -= rain_changed*0.1
+        reservoir_changed += rain_changed
+        reservoir_changed += reservoir_changed*0.05
+        reservoir_changed -= reservoir_changed*0.02
+        reservoir_changed -= 2.5e5
+        print(f"El volumen del reservoirorio es {reservoir_changed}m3")
 
-new_volume(reservoir=4.445e8, rain=5e6)
+change_volume = WaterReservoir(reservoir=4.445e8, rain=5e6).new_volume()
