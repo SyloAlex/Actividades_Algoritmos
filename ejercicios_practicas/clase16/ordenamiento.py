1b985cab5a07f380c2f0b7732ce0a7f0b41abcf8
Alex Mena <alejandromena@correo.unimet.edu.ve>
2021-06-24 12:10:59 -0400

    Practica 16 terminada

--- ejercicios_practicas/clase16/ordenamiento.py
+++ ejercicios_practicas/clase16/ordenamiento.py
@@ -18,15 +18,51 @@ def insertion_sort(vector, compare=lambda x,y: x < y):
 #insercion, seleccion y mezcla en ordenar un vector de
 #10.000 posiciones
 
+def merge(left, right):
+    if len(left) == 0:
+        return right
+    if len(right) == 0:
+        return left
+    result = []
+    index_left = index_right = 0
+    while len(result) < len(left) + len(right):
+        if left[index_left] <= right[index_right]:
+            result.append(left[index_left])
+            index_left += 1
+        else:
+            result.append(right[index_right])
+            index_right += 1
+        if index_right == len(right):
+            result += left[index_left:]
+            break
+        if index_left == len(left):
+            result += right[index_right:]
+            break
+    return result
+
+def merge_sort(array):
+    if len(array) < 2:
+        return array
+
+    midpoint = len(array) // 2
+    return merge(
+        left=merge_sort(array[:midpoint]),
+        right=merge_sort(array[midpoint:]))
+
 array = []
-for i in range(100):
+for i in range(10000):
     while True:
-        number = random.randint(0, 100)
+        number = random.randint(0, len(range(10000)) - 1)
         if number not in array:
             array.append(number)
             break
 
 start = time.time()
-insertion_sort(array)
+print("Insertion>>", insertion_sort(array))
 end = time.time()
-print(end-start)
+print(end-start, "segundos")
+
+start1 = time.time()
+print("Merge>>", merge_sort(array))
+end1 = time.time()
+print(end1-start1, "segundos")
